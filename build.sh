047ab1b7731232663082d6156a6952cfb94b3137
New Future <NewFuture@users.noreply.github.com>
2025-05-06 11:46:21 +0000

    build and update.jsob

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ ABI=$1
 # 从 magisk-rclone/module.prop 文件中读取 RCLONE_VERSION
 RCLONE_VERSION=$(grep -oP '^version=\Kv.*' magisk-rclone/module.prop)
 
+# 复制目录并准备环境
 cp magisk-rclone magisk-rclone_$ABI -r
 
 ./scripts/download-rclone.sh $ABI $RCLONE_VERSION magisk-rclone_$ABI/system/vendor/bin/rclone
@@ -15,6 +16,23 @@ cp magisk-rclone magisk-rclone_$ABI -r
 cp libfuse/build/util/fusermount3 magisk-rclone_$ABI/system/vendor/bin/
 chmod +x magisk-rclone_$ABI/system/vendor/bin/*
 
+# 修改 module.prop 中的 updateJson 字段
+UPDATE_JSON_URL="https://github.com/NewFuture/rclone-fuse3-magisk/releases/latest/download/update-$ABI.json"
+sed -i "s|^updateJson=.*|updateJson=$UPDATE_JSON_URL|" magisk-rclone_$ABI/module.prop
+
+# 生成对应的 update.json 文件
+cat <<EOF > update-$ABI.json
+{
+  "version": "$RCLONE_VERSION",
+  "versionCode": $VERSION_CODE,
+  "zipUrl": "https://github.com/NewFuture/rclone-fuse3-magisk/releases/latest/download/magisk-rclone_$ABI.zip",
+  "changelog": "https://github.com/NewFuture/rclone-fuse3-magisk/releases/tag/$RCLONE_VERSION"
+}
+EOF
+
+echo "生成的 update.json 文件: update-$ABI.json"
+
+# 打包 ZIP 文件
 cd magisk-rclone_$ABI
 mkdir -p META-INF/com/google/android
 echo "#MAGISK" > META-INF/com/google/android/updater-script
